25bb7d065b8b41985abfa72fcd0db339b66a826c
jmau949 <44676775+jmau949@users.noreply.github.com>
2021-03-10 00:59:57 -0600

    remote auth

--- secondproj/container/config/webpack.prod.js
+++ secondproj/container/config/webpack.prod.js
@@ -1,33 +1,32 @@
-const { merge } = require('webpack-merge')
-const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
-const commonConfig = require('./webpack.common')
-const packageJson = require('../package.json');
-
+const { merge } = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
 
 const domain = process.env.PRODUCTION_DOMAIN;
 
-
 const prodConfig = {
-    mode: 'production',
-    output: {
-        // done for caching
-        filename: '[name].[contenthash].js',
-        // publicpath error unexpected token '<'
-        // used anytime when you have some part of webpack that tries to refer to a file that has been built by webpack
-        // whenever html plugin tries to refer to some js file that has been created
-        // right now, whenever html plugin tries to refer to js file built by webpack, it makes use of direct file name
-        // public path to add a path
-        publicPath: '/container/latest/'
-    },
-    plugins: [
-        new ModuleFederationPlugin({
-            name: 'container', 
-            remotes: {
-                marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`
-            },
-            shared: packageJson.dependencies
-        })
-    ]
-}
+  mode: "production",
+  output: {
+    // done for caching
+    filename: "[name].[contenthash].js",
+    // publicpath error unexpected token '<'
+    // used anytime when you have some part of webpack that tries to refer to a file that has been built by webpack
+    // whenever html plugin tries to refer to some js file that has been created
+    // right now, whenever html plugin tries to refer to js file built by webpack, it makes use of direct file name
+    // public path to add a path
+    publicPath: "/container/latest/",
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "container",
+      remotes: {
+        marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
+        auth: `auth@${domain}/auth/latest/remoteEntry.js`,
+      },
+      shared: packageJson.dependencies,
+    }),
+  ],
+};
 
-module.exports = merge(commonConfig, prodConfig)
+module.exports = merge(commonConfig, prodConfig);
